be680946ebacb1e61fe2ff06ce627215526bc6d3
ze-martin <ze-martin@users.noreply.github.com>
2026-09-05 19:25:17 -0500

    Forzar actualizacion de cache Betano

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protocolo-apuestas-shell-v2'
+const CACHE_NAME = 'protocolo-apuestas-shell-v3'
 const SHELL_ASSETS = ['./', './manifest.webmanifest', './favicon.svg']
 
 self.addEventListener('install', (event) => {
